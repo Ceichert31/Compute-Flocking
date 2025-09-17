a654e990a84f4adc8ee21817ede3f1d1b2b2a0a2
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-09-17 11:05:12 -0400

    refactor: Changed to cardinal direction based movement

--- ComputeShaderTest/Assets/MazeGeneration/Scripts/RandomizedMazeGeneration.cs
+++ ComputeShaderTest/Assets/MazeGeneration/Scripts/RandomizedMazeGeneration.cs
@@ -33,10 +33,10 @@ public class RandomizedMazeGeneration : MonoBehaviour
     [SerializeField]
     private int height = 10;
 
-    private Dictionary<Vector2, Vertex> mazeMap = new Dictionary<Vector2, Vertex>();
+    private Dictionary<Vector2, Cell> mazeMap = new Dictionary<Vector2, Cell>();
     private Vector2 key;
     
-    private Stack<Vertex> path = new Stack<Vertex>();
+    private Stack<Cell> path = new Stack<Cell>();
     private void Start()
     {
         PopulateMaze();
@@ -51,7 +51,10 @@ public class RandomizedMazeGeneration : MonoBehaviour
             for (int y = 0; y < Height; ++y)
             {
                 key.Set(x, y);
-                mazeMap[key] = new Vertex();
+                mazeMap[key] = new Cell
+                {
+                    position = new Vector2(x, y)
+                };
             }
         }
     }
@@ -60,24 +63,88 @@ public class RandomizedMazeGeneration : MonoBehaviour
     {
         //Mark top cell as visited
         key.Set(0, 0);
-        Vertex currentVertex = mazeMap[key];
-        path.Push(currentVertex);
+        Cell currentCell = mazeMap[key];
+        path.Push(currentCell);
         
         //Iterate until we have nothing left to add to stack
         while (path.Count > 0)
         {
-            var neighbor = GetNeighbors((int)key.x, (int)key.y);
+            //Mark as visited and add to stack
+            currentCell.isVisited = true;
+            path.Push(currentCell);
+            
+            List<Cell> neighbors = new List<Cell>();
 
-            //Case where we have a neighbor
-            if (neighbor != null)
+            Vector2 up = new Vector2(key.x, key.y + 1);
+            Vector2 right = new Vector2(key.x + 1, key.y);
+            Vector2 down = new Vector2(key.x, key.y - 1);
+            Vector2 left = new Vector2(key.x - 1, key.y);
+
+            //Add all visitable neighbors to list
+            if (!mazeMap[up].isVisited && mazeMap[up].down)
             {
-                //neighbor.
+                neighbors.Add(mazeMap[up]);
+            } 
+            if (!mazeMap[right].isVisited && mazeMap[right].left)
+            {
+                neighbors.Add(mazeMap[right]);
+            }
+            if (!mazeMap[down].isVisited && mazeMap[down].up)
+            {
+                neighbors.Add(mazeMap[down]);
+            }
+            if (!mazeMap[left].isVisited && mazeMap[left].right)
+            {
+                neighbors.Add(mazeMap[left]);
+            }
+            
+            //Case where we have a neighbor
+            if (neighbors.Count > 0)
+            {
+                //Get a random neighbor
+                int index = Random.Range(0, neighbors.Count);
+                Cell oldCell = currentCell;
+                currentCell = neighbors[index];
+                
+                RemoveWalls(oldCell, currentCell);
+                currentCell.isVisited = true;
             }
             //Backtracking
             else
             {
-                path.Pop();
+                currentCell = path.Pop();
             }
+        }
+    }
+    
+    private void RemoveWalls(Cell current, Cell next)
+    {
+        int distX = (int)(current.position.x - next.position.x);
+        int distY = (int)(current.position.y - next.position.y);
+
+        //Moving up
+        if (distY == 1)
+        {
+            current.up = false;
+            next.down = false;
+        }
+        //Moving right
+        if (distX == 1)
+        {
+            current.right = false;
+            next.left = false;
+        }
+        //Moving down
+        if (distY == -1)
+        {
+            current.down = false;
+            next.up = false;
+        }
+        //Moving left
+        if (distX == -1)
+        {
+            current.left = false;
+            next.right = false;
         }
     }
 
@@ -85,9 +152,9 @@ public class RandomizedMazeGeneration : MonoBehaviour
     /// Gets all neighbors, and chooses one randomly
     /// </summary>
     /// <returns></returns>
-    private Vertex GetNeighbors(int x, int y)
+    private Cell GetNeighbors(int x, int y)
     {
-        List<Vertex> neighbors = new List<Vertex>();
+        List<Cell> neighbors = new List<Cell>();
 
         Vector2 up = new Vector2(x, y + 1);
         Vector2 down = new Vector2(x, y - 1);
@@ -127,15 +194,12 @@ public class RandomizedMazeGeneration : MonoBehaviour
     }
 }
 
-class Vertex
+class Cell
 {
-    public Edge vertical;
-    public Edge horizontal;
     public Vector2 position;
-}
-
-struct Edge
-{
+    public bool up = true;
+    public bool right = true;
+    public bool down = true;
+    public bool left = true;
     public bool isVisited;
-    public bool wallDestroyed;
 }
